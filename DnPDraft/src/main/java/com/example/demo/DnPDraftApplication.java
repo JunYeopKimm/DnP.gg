16e91e585d588f5c29ac7be40db6de80c2ca0dab
GEN947 <leavegood1@gmail.com>
2024-05-17 13:31:15 +0900

    ㅇ

---
+++ DnPDraft/src/main/java/com/example/demo/DnPDraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnPDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnPDraftApplication.class, args);
+	}
+
+}
